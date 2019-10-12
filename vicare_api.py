20dfe53c2974ebbbc829766f4ae29e58332fc12c
Radek <radoslaw.zdunski@gmail.com>
2019-10-12 22:52:34 +0200

    Deleted objects returing errors

--- vicare_api.py
+++ vicare_api.py
@@ -8,36 +8,22 @@ from PyViCare.PyViCare import ViCareSession
 from dane import dane
 t=ViCareSession(*dane)
 
-print("DomesticHotWaterConfiguredTemperature: ", t.getDomesticHotWaterConfiguredTemperature()) 
 
+#EXAMPLE: GETTING SETTINGS
+print("DomesticHotWaterConfiguredTemperature: ", t.getDomesticHotWaterConfiguredTemperature()) 
 print("Outside_Temperature: ", t.getOutsideTemperature())
 print("RoomTemperature: ", t.getRoomTemperature())
-#print("Supply_Temperature: ", t.getSupplyTemperature())
 print("HeatingCurveShift: ", t.getHeatingCurveShift()) 
 print("HeatingCurveSlope: ", t.getHeatingCurveSlope()) 
-#print("BoilerTemperature:", t.getBoilerTemperature())
 print("ActiveProgram: ", t.getActiveProgram())
-
-# print("Programs: ", t.getPrograms())
-
 print("CurrentDesiredTemperature: ", t.getCurrentDesiredTemperature())
-#print("MonthSinceLastService: ", t.getMonthSinceLastService())
-#print("LastServiceDate: ", t.getLastServiceDate())
-
 print("DesiredTemperatureForProgram: ", t.getDesiredTemperatureForProgram("comfort"))
 print("ActiveMode: ", t.getActiveMode())
-
 print("DesiredTemperatureForProgram: ", t.getDesiredTemperatureForProgram("comfort"))
+
+#EXAMPLE SETTING SETTINGS
 print("ProgramTemperature: ", t.setProgramTemperature("comfort",23))
-#print("activateProgram after set: ", t.activateProgram("comfort"))
 print("DomesticHotWaterTemperature_SET: ", t.setDomesticHotWaterTemperature(40))
-#print("activateProgram: ", t.activateProgram("comfort"))
 print("deactivateProgram: ", t.deactivateComfort())
-print("CurrentPower: ", t.getCurrentPower())
-print(t.getCirculationPumpActive())
-print(t.getBurnerModulation())
-print(t.getGasConsumptionHeatingYears())
-print("GasConsumptionHeatingThisMonth: ", t.getGasConsumptionHeatingThisMonth())
-print(t.getGasConsumption())
 
 
